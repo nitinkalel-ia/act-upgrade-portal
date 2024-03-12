d8a5d83099f2dd4456f86cf9d1e002ad7d9a6d2c
PratikInteractive <pratik.keluskar@interactiveavenues.com>
2024-03-12 09:50:17 +0530

    Pratik-IA/Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule , ReactiveFormsModule  } from '@angular/forms';
-import { LoginModule } from './login/login.module';
+import { ReactiveFormsModule  } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule,
-    LoginModule,
     ReactiveFormsModule
   ],
-  bootstrap: [LoginComponent]
+  bootstrap: []
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  imports:[FormsModule , CommonModule],
+  standalone: true
 })
 export class LoginComponent implements OnInit {
   responseData: any;
   postData: any = {
-    account_no: '',  
+    account_no: '102016461638',  
     pkg_cat_id: '68',
     bundle: 'netflix',
   };
@@ -20,11 +25,14 @@ export class LoginComponent implements OnInit {
 
   submitForm() {
     console.log(this.postData.account_no)
+    
   
     this.http.post('https://betafibernet.actcorp.in/plan_change/Plans/Login', this.postData).subscribe(
       (data) => {
         this.responseData = data;
         console.log(data);
+        console.log('Token:', this.responseData.data.token);
+        
       },
       (error) => {
         console.error('Error fetching data:', error);
